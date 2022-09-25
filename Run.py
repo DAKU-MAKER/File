355afa04a0249bf98760dd379abdaf144f10c450
DAKU-MAKER <104707533+DAKU-MAKER@users.noreply.github.com>
2022-09-25 15:38:56 +0530

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,2 +1,10 @@
-from nb1 import make
-make()
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from nb1 import make
+    make()
